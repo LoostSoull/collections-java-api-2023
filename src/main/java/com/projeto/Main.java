5699f51b3aae65849b0f317bbba11e14de4a81c6
lucas silva <lucas.silva.cardoso1996@gmail.com>
2023-08-17 00:46:00 -0300

    teste Intelliji com github

--- src/main/java/com/projeto/Main.java
+++ src/main/java/com/projeto/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.projeto;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -14,6 +14,7 @@ public class Main {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
+            //teste
             System.out.println("i = " + i);
         }
     }
