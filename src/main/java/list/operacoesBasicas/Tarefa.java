957e8380e417a2c3323db969646c66110deebfc9
lucas silva <lucas.silva.cardoso1996@gmail.com>
2023-08-19 03:19:41 -0300

    finalizado exercicio da lista de tarefas

---
+++ src/main/java/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,43 @@
+package list.operacoesBasicas;
+
+import list.ListaTarefa;
+
+public class Tarefa {
+    //atributos
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    @Override
+    public String toString() {
+        return descricao ;
+    }
+
+    /*
+
+    como nao existe teste unitario, realizado teste das funções nesse bloco de codigo
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+
+
+        listaTarefa.adicionarTarefa("tarefa1");
+        listaTarefa.adicionarTarefa("tarefa1");
+        listaTarefa.adicionarTarefa("tarefa2");
+        System.out.println("numero de tarefas na lista: " + listaTarefa.obterNumeroTotalTarefas());
+
+       listaTarefa.removerTarefa("tarefa2");
+        System.out.println("numero de tarefas na lista: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+    }*/
+}
